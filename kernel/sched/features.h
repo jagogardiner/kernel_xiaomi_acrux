afe258f32906b77e1ba11a59acd30debd9aa6798
Peter Zijlstra <peterz@infradead.org>
2017-03-01 11:24:35 +0100

    sched/fair: Make select_idle_cpu() more aggressive
    [ Upstream commit 4c77b18cf8b7ab37c7d5737b4609010d2ceec5f0 ]
    
    Kitsunyan reported desktop latency issues on his Celeron 887 because
    of commit:
    
      1b568f0aabf2 ("sched/core: Optimize SCHED_SMT")
    
    ... even though his CPU doesn't do SMT.
    
    The effect of running the SMT code on a !SMT part is basically a more
    aggressive select_idle_cpu(). Removing the avg condition fixed things
    for him.
    
    I also know FB likes this test gone, even though other workloads like
    having it.
    
    For now, take it out by default, until we get a better idea.
    
    Reported-by: kitsunyan <kitsunyan@inbox.ru>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Chris Mason <clm@fb.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Mike Galbraith <umgwanakikbuti@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Change-Id: I7d6da4ff90413e65ff955c7047c6d065cafa303f
    
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -64,6 +64,11 @@ SCHED_FEAT(TTWU_QUEUE, true)
  */
 SCHED_FEAT(WARN_DOUBLE_CLOCK, false)
 
+/*
+ * When doing wakeups, attempt to limit superfluous scans of the LLC domain.
+ */
+SCHED_FEAT(SIS_AVG_CPU, false)
+
 #ifdef HAVE_RT_PUSH_IPI
 /*
  * In order to avoid a thundering herd attack of CPUs that are
