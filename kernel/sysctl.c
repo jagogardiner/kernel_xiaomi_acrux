2c69e277d86d7b1d6e86eced6b43ede5b60a9a34
Tyler Nijmeh <tylernij@gmail.com>
2019-05-13 15:52:50 -0700

    kernel/sysctl: Fix misc sched tunables not being in macro check
    These sched tunables shouldn't be exposed if sysctl for the scheduler is
    off. Place these sched tunables here.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Jago Gardiner (nysadev) <jagogardiner@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -457,7 +457,6 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &one,
 	},
 #endif /* CONFIG_NUMA_BALANCING */
-#endif /* CONFIG_SCHED_SYSCTL */
 	{
 		.procname	= "sched_rt_period_us",
 		.data		= &sysctl_sched_rt_period,
@@ -515,6 +514,7 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &one_hundred,
 	},
 #endif
+#endif /* CONFIG_SCHED_SYSCTL */
 #ifdef CONFIG_PROVE_LOCKING
 	{
 		.procname	= "prove_locking",
