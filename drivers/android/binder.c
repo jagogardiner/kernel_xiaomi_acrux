e10084a6ef9e499f1b14ece3e6464ee1a1958d4a
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-12 11:57:52 -0700

    binder: Reserve caches for small, high-frequency memory allocations
    Most of binder's memory allocations are tiny, and they're allocated
    and freed extremely frequently. The latency from going through the page
    allocator all the time for such small allocations ends up being quite
    high, especially when the system is low on memory. Binder is
    performance-critical, so this is suboptimal.
    
    Instead of using kzalloc to allocate a struct every time, reserve caches
    specifically for allocating each struct quickly.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Jago Gardiner (nysadev) <nysadev@raphielgang.org>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5995,9 +5995,7 @@ static int __init binder_init(void)
 	if (ret)
 		return ret;
 
-        ret = binder_alloc_shrinker_init();
-        if (ret)
-                return ret;
+	binder_alloc_shrinker_init();
 
 	atomic_set(&binder_transaction_log.cur, ~0U);
 	atomic_set(&binder_transaction_log_failed.cur, ~0U);
