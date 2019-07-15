0220189970541984f3557ca59d0a045f8a8816c9
Jago Gardiner (nysadev) <jagogardiner@gmail.com>
2019-07-15 11:38:52 +0100

    qcacld-3.0: Fix bruh
    Signed-off-by: Jago Gardiner (nysadev) <jagogardiner@gmail.com>

--- drivers/staging/qcacld-3.0/core/mac/inc/mac_trace.h
+++ drivers/staging/qcacld-3.0/core/mac/inc/mac_trace.h
@@ -17,13 +17,9 @@
  */
 
 /**=========================================================================
-
  *  \file  mac_trace.h
-
  *  \brief definition for trace related APIs
-
    \author Sunit Bhatia
-
    ========================================================================*/
 
 #ifndef __MAC_TRACE_H
@@ -33,11 +29,6 @@
 
 #define MAC_TRACE_GET_MODULE_ID(data) ((data >> 8) & 0xff)
 #define MAC_TRACE_GET_MSG_ID(data)       (data & 0xffff)
-
-QDF_STATUS pe_acquire_global_lock(tAniSirLim *psPe);
-QDF_STATUS pe_release_global_lock(tAniSirLim *psPe);
-
-#ifdef TRACE_RECORD
 
 #define eLOG_NODROP_MISSED_BEACON_SCENARIO 0
 #define eLOG_PROC_DEAUTH_FRAME_SCENARIO 1
@@ -52,13 +43,14 @@ uint8_t *mac_trace_get_lim_msg_string(uint16_t limMsg);
 uint8_t *mac_trace_get_wma_msg_string(uint16_t wmaMsg);
 uint8_t *mac_trace_get_sme_msg_string(uint16_t smeMsg);
 uint8_t *mac_trace_get_info_log_string(uint16_t infoLog);
+QDF_STATUS pe_acquire_global_lock(tAniSirLim *psPe);
+QDF_STATUS pe_release_global_lock(tAniSirLim *psPe);
 uint8_t *mac_trace_get_neighbour_roam_state(uint16_t neighbourRoamState);
 uint8_t *mac_trace_getcsr_roam_state(uint16_t csr_roamState);
 uint8_t *mac_trace_getcsr_roam_sub_state(uint16_t csr_roamSubState);
 uint8_t *mac_trace_get_lim_sme_state(uint16_t limState);
 uint8_t *mac_trace_get_lim_mlm_state(uint16_t mlmState);
 uint8_t *mac_trace_get_tl_state(uint16_t tlState);
-
 #else
 static inline void mac_trace(tpAniSirGlobal pMac, uint8_t code, uint16_t session,
 			     uint32_t data)
