c62cf49c2dbf4a08a00e24b43abe6b313e1d9f0e
Jago Gardiner (nysadev) <nysadev@raphielgang.org>
2019-07-17 10:51:23 +0100

    sched/fair: Fix mismerge
    Signed-off-by: Jago Gardiner (nysadev) <nysadev@raphielgang.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2651,6 +2651,14 @@ int sched_set_wake_up_idle(struct task_struct *p, int wake_up_idle)
 }
 EXPORT_SYMBOL(sched_set_wake_up_idle);
 
+int core_ctl_set_boost(bool boost)
+{
+	return 0;
+}
+EXPORT_SYMBOL(core_ctl_set_boost);
+
+#ifdef CONFIG_SMP
+
 /*
  * Approximate:
  *   val * y^n,    where y^32 ~= 0.5 (~1 scheduling period)
