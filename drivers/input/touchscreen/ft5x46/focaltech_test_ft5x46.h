2260bd555bb841639b34950f2fdbc5eea372cff3
nysascape <jago@nysascape.digital>
2020-03-11 21:57:44 +0000

    treewide: Cleanup a lot of Xiaomi changes
    Signed-off-by: nysascape <jago@nysascape.digital>
    Change-Id: Ied994b805731335306cf9356e3d866819eb5bded

--- drivers/input/touchscreen/ft5x46/focaltech_test_ft5x46.h
+++
@@ -1,3 +0,0 @@
-unsigned char FT5X46_TestItem_RawDataTest(struct i2c_client *client);
-unsigned char FT5X46_TestItem_WeakShortTest(struct i2c_client *client);
-int FT5X46_TestItem_GetRawData(u8 *buff, int type);
